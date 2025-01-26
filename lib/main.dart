1fbe6c24277e100fcd90f656392541d79b358302
宮田蓮 <miyataren@miyatarennoMacBook-Air.local>
2025-01-26 22:46:08 +0900

    ElevatedButton custom

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_alarm/sign_up.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const SignUpScreen(),
     );
   }
 }
