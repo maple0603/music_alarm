bdfa687e68f4e3595d459766d9212c8417a5ea02
宮田蓮 <miyataren@miyatarennoMacBook-Air.local>
2025-01-30 17:16:27 +0900

    start_screen_sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:music_alarm/setting.dart';
+import 'package:music_alarm/start_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const SignUpScreen(),
-      home: const SettingScreen(),
+      home: const StartScreen(),
     );
   }
 }
